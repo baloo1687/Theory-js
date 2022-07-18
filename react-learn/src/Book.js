d64ca57817326c0f48c800e6f43f149027bed5f0
Artem Pylypchuk <artem.20.2016@gmail.com>
2022-07-18 18:11:34 +0300

    Added react theory

---
+++ react-learn/src/Book.js
@@ -0,0 +1,11 @@
+const Book = (props) => {
+    return (
+        <div>
+        <h2>{props.name}</h2>
+        <p>{props.year}</p>
+        <p>{props.price}</p>
+        </div>
+    );
+};
+
+export default Book;
